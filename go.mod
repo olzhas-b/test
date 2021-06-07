97b1f9e1641c445d503604a087e2ed2ecaf4eef2
olzhas-b <oljas.bazarbekov15@gmail.com>
2021-06-08 01:06:13 +0600

    v1.6.0

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/olzhas-b/test
 
 go 1.16
-
-require github.com/gin-gonic/gin v1.7.2
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 func sum(a, b int) int {
 	return a + b
 }
+
 func main() {
 	println(sum(5, 10))
+	println(Sum(2, 3))
 }
